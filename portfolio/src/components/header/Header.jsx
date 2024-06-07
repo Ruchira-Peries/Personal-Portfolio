dc06a60593e45f8ffc339f8dbd8bc00133b560a9
101Luna <antonyperies99@gmail.com>
2024-06-08 03:25:45 +0530

    created the home section

--- portfolio/src/components/header/Header.jsx
+++ portfolio/src/components/header/Header.jsx
@@ -16,31 +16,31 @@ const Header = () => {
                 </li>
 
                  <li className='nav__item'>
-                    <a href='#about' className='nav__link'>
+                    <a href='#about' className='nav__link active-link'>
                         <i className='uil uil-user nav__icon'></i>About
                     </a>
                 </li>
 
                  <li className='nav__item'>
-                    <a href='#skills' className='nav__link'>
+                    <a href='#skills' className='nav__link active-link'>
                         <i className='uil uil-file-alt nav__icon'></i>Skills
                     </a>
                 </li>
 
                  <li className='nav__item'>
-                    <a href='#services' className='nav__link'>
+                    <a href='#services' className='nav__link active-link'>
                         <i className='uil uil-briefcase-alt nav__icon'></i>Services
                     </a>
                 </li>
 
                  <li className='nav__item'>
-                    <a href='#portfolio' className='nav__link'>
+                    <a href='#portfolio' className='nav__link active-link'>
                         <i className='uil uil-scenery nav__icon'></i>Portfolio
                     </a>
                 </li>
             
                  <li className='nav__item'>
-                    <a href='#contact' className='nav__link'>
+                    <a href='#contact' className='nav__link active-link'>
                         <i className='uil uil-message nav__icon'></i>Contact
                     </a>
                 </li> 
