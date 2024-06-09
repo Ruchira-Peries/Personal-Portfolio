10925ae2ffdac8af0493996f6f1d63379c5a0d1f
101Luna <antonyperies99@gmail.com>
2024-06-10 00:04:45 +0530

    Updated the skills section

---
+++ portfolio/src/components/skills/Technologies.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+const Technologies = () => {
+  return (
+       <div className='skills__content'>
+        <h3 className='skills__title'>Technologies</h3>
+
+        <div className='skills__box'>
+            <div className='skills__group'>
+                <div className='skills__data'>
+                    <i class='bx bx-badge-check'></i>
+
+                    <div>
+                        <h3 className='skills__name'>Figma</h3>
+                        <span className='skills__level'>Basic</span>
+                    </div>
+                </div>
+
+                 <div className='skills__data'>
+                    <i class='bx bx-badge-check'></i>
+
+                    <div>
+                        <h3 className='skills__name'>Flutter</h3>
+                        <span className='skills__level'>Basic</span>
+                    </div>
+                </div>
+
+                 <div className='skills__data'>
+                    <i class='bx bx-badge-check'></i>
+
+                    <div>
+                        <h3 className='skills__name'>GitHub</h3>
+                        <span className='skills__level'>Basic</span>
+                    </div>
+                </div>
+
+                 <div className='skills__data'>
+                    <i class='bx bx-badge-check'></i>
+
+                    <div>
+                        <h3 className='skills__name'>Photoshop</h3>
+                        <span className='skills__level'>Basic</span>
+                    </div>
+                </div>
+
+                 <div className='skills__data'>
+                    <i class='bx bx-badge-check'></i>
+
+                    <div>
+                        <h3 className='skills__name'>Illustrator</h3>
+                        <span className='skills__level'>Basic</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Technologies
