eb768de984a895697f106b82b725ac07c6953ac9
101Luna <antonyperies99@gmail.com>
2024-06-15 23:22:45 +0530

    Added the footer section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,6 +7,7 @@ import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
 import Qualifications from './components/qualifications/Qualifications';
 import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Qualifications/>
           <Contact/>
         </main>
+          <Footer/>
       </>
   );
 }
