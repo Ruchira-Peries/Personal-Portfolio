325de42b750c09fd2da381671b06ca0d8a8ddc8f
101Luna <antonyperies99@gmail.com>
2024-06-22 22:39:07 +0530

    Did some changes in styling

--- portfolio/src/components/work/WorkItems.jsx
+++ portfolio/src/components/work/WorkItems.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { projectsData } from './Data';
 
 const WorkItems = ({item}) => {
   return (
     <div className='work__card' key={item.id}>
       <img src={item.image} alt='' className='work__img'/>
       <h3 className='work__title'>{item.title}</h3>
-      <a href='#' className='work__button'>
-        Demo<i className='bx bx-right-arrow-alt work__button-icon'></i>
-      </a>
+      <p className='work__subtitle'>{item.subtitle}</p>
+      <p className='work__technologies'>{item.technologies}</p>
+      <div className='work__icons'>
+             {item.icons.map((icons, index) => (
+                <a key={index} className='work__social' href={icons.href} target="_blank" rel="noopener noreferrer">
+                    <i className={icons.iconClass}></i>
+                </a>
+            ))}
+      </div>
     </div>
   )
 }
